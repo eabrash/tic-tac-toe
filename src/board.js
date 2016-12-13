7e7b514fbdfb0f8901c48c212c152eea1b8a7462
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-13 13:35:16 -0800

    added setMarkAtPosition method, clarified board attribute

--- src/board.js
+++ src/board.js
@@ -3,8 +3,21 @@
 
 var Board = function() {
 
+	this.board = [[null, null, null], 
+								[null, null, null], 
+								[null, null, null]];
 };
 
+Board.prototype.setMarkAtPosition = function(row, column, mark) {
+	if (row > 2 || row < 0 || column > 2 || column < 0 || this.board[row][column] != null ) {
+		throw "Try Again!";
+	} else {
+		this.board[row][column] = mark;
+	};
+};
 
+Board.prototype.getMarkAtPosition = function(row, column) {
+	return this.board[0][0]
+};
 
 export default Board;
