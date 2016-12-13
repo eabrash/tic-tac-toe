ac98f7a760ee0ae77a842364c7727b5d8c17e6a5
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-12 17:11:28 -0800

    added error for setCurrentPlayer, created method setCurrentPlayer

--- src/game.js
+++ src/game.js
@@ -5,20 +5,28 @@ var Game = function() {
 };
 
 Game.prototype.checkWinStatus = function() {
+	//will involve logic and Board and choosing Squares etc etc
 	return this.winStatus;
 };
 
 Game.prototype.getCurrentPlayer = function() {
-
+	return this.currentPlayer;
 };
 
-Game.prototype.setCurrentPlayer = function() {
+Game.prototype.setCurrentPlayer = function( player ) {
+	if (player == "") {
+		throw 42
+	} else {
+		this.currentPlayer = player;
+	};
 
 };
 
 Game.prototype.setPlayers = function(player1, player2) {
 	this.player1 = player1;
 	this.player2 = player2;
+	//player1 starts the game
+	this.setCurrentPlayer(player1);
 	return [this.player1, this.player2];
 };
 
