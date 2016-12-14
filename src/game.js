364cd2caad7a899e6bdc061cd2512ec035ce966c
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-14 12:53:58 -0800

    linked winning to values in a players attribute - magic box

--- src/game.js
+++ src/game.js
@@ -15,6 +15,9 @@ Game.prototype.checkWinStatus = function() {
 		this.winStatus = "draw";
 	};
 
+	if (this.currentPlayer.checkPoints == true) {
+		this.winStatus = this.currentPlayer.mark;
+	}
 	return this.winStatus;
 }; // END checkWinStatus
 
@@ -48,16 +51,18 @@ Game.prototype.setPlayers = function(player1, player2) {
 Game.prototype.takeTurns = function(row, col) {
 	this.currentPlayer.chooseSquare(row, col);
 
+	if (this.board.playCounter >= 5) {
+		console.log(this.checkWinStatus());
+
+	};
+
 	if (this.currentPlayer == this.player1) {
 		this.currentPlayer = this.player2;
 	} else if (this.currentPlayer == this.player2 ) {
 		this.currentPlayer = this.player1;
 	};
 
-	if (this.board.playCounter >= 5) {
-		console.log(this.checkWinStatus());
 
-	};
 	this.board.drawBoard();
 }; //END togglePlayer
 
--- src/player.js
+++ src/player.js
@@ -6,6 +6,14 @@ var Player = function() {
 	this.name = '';
 	this.mark = '';
 	this.status = 'inactive';
+	this.row1 = 0;
+	this.row2 = 0;
+	this.row3 = 0;
+	this.col1 = 0;
+	this.col2 = 0;
+	this.col3 = 0;
+	this.diagonalL2R = 0;
+	this.diagonalR2L = 0;
 };
 
 Player.prototype.getName = function() {
@@ -38,10 +46,68 @@ Player.prototype.setCurrentGame = function(game) {
 
 
 //can't do until board is made
-Player.prototype.chooseSquare = function(row, column) {
-	this.currentGame.board.setMarkAtPosition(row, column, this.mark);
-	// console.log(this.game.board.board);
-};
+Player.prototype.chooseSquare = function(row, col) {
+	this.currentGame.board.setMarkAtPosition(row, col, this.mark);
+	var points = this.getPoints( row, col );
+
+	if (row == 0) {
+		this.row0 += points;
+	} else if (row == 1) {
+		this.row1 += points;
+	} else if (row == 2) {
+		this.row2 += points;
+	};
+
+	if (col == 0 ) {
+		this.col0 += points;
+	} else if (col == 1 ) {
+		this.col1 += points;
+	} else if (col == 2) {
+		this.col2 += points;
+	};
+
+	if ((row == 0 && col == 0 ) || (row == 1 && col == 1 ) || (row == 2 && col == 2 )) {
+		this.diagonalL2R += points;
+	};
+
+	if ((row == 0 && col == 2 ) || (row == 1 && col == 2 ) || (row == 2 && col == 0 )) {
+		this.diagonalR2L += points;
+	};
+}; //chooseSquare
+
+Player.prototype.checkPoints = function() {
+	if (this.row0 == 15 || this.row1 == 15 || this.row2 == 15 || this.col0 == 15 || this.col1 == 15 || this.col2 == 15 || this.diagonalR2L == 15 || this.diagonalL2R == 15) {
+		return true;
+	};
+	return false;
+}
+
+Player.prototype.getPoints = function( row, col ) {
+	var points = 0
+
+	if (row == 0 && col == 0) {
+		points = 8;
+	} else if (row == 0 && col == 1 ) {
+		points = 1;
+	} else if (row == 0 && col == 2 ) {
+		points = 6;
+	} else if (row == 1 && col == 0 ) {
+		points = 3;
+	} else if (row == 1 && col == 1 ) {
+		points = 5;
+	} else if (row == 1 && col == 2 ) {
+		points = 7;
+	} else if (row == 2 && col == 0 ) {
+		points = 4;
+	} else if (row == 2 && col == 1 ) {
+		points = 9;
+	} else if (row == 2 && col == 2 ) {
+		points = 2;
+	};
+	return points;
+}
+
+
 
 // shows whether a player is active and can choose a square or not
 
